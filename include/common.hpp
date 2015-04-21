6823e675ffd77052390d78e026bffd4310355b5e
Nico Previtali <pnico@student.ethz.ch>
2015-04-21 15:06:17 +0200

    Removed MSVS13 compilation issues

--- include/common.hpp
+++ include/common.hpp
@@ -21,12 +21,16 @@
 #if defined(PLATFORM_APPLE)
 	#define GLFW_INCLUDE_GLCOREARB
 #elif defined(PLATFORM_WINDOWS)
+	#define NOMINMAX
 	#include <windows.h>
     #define GLEW_STATIC
     #include <GL/glew.h>
+	#define _USE_MATH_DEFINES
 #else
     #define GL_GLEXT_PROTOTYPES
 #endif
+#include <memory>
+#include <math.h>
 #include "GLFW/glfw3.h"
 #include "tinyformat.h"
 
